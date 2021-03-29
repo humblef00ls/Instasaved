e547571b129df2a793acd2c87c4b2040a25f5f8a
Aparimeya Taneja <at307@duke.edu>
2021-03-29 12:36:47 -0400

    frontend

---
+++ frontend/svelte.config.cjs
@@ -0,0 +1,27 @@
+const static = require('@sveltejs/adapter-static');
+const pkg = require('./package.json');
+
+module.exports = {
+	// options passed to svelte.compile (https://svelte.dev/docs#svelte_compile)
+	compilerOptions: null,
+
+	// an array of file extensions that should be treated as Svelte components
+	extensions: ['.svelte'],
+
+	kit: {
+		adapter: static(),
+
+
+		files: {
+			assets: 'static',
+			lib: 'src/lib',
+			routes: 'src/routes',
+			serviceWorker: 'src/service-worker',
+			template: 'src/app.html'
+		},
+
+
+	},
+
+
+};
